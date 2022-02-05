8844941a3bc36dcfe404f65b2231c71c5abc9aa1
Max Blomstervall <max.blomstervall@gmail.com>
2022-02-05 13:19:49 +0100

    feat: new schema of branch per pc, nix conf added

---
+++ nixos/configuration.nix
@@ -0,0 +1,148 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  # boot.kernelPackages = linuxPackages_latest;
+
+  networking.hostId = "ab79c42d";
+  networking.hostName = "home-station"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp3s0.useDHCP = true;
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+
+  # Enable the GNOME 3 Desktop Environment.
+  services = {
+    xserver = {
+      enable = true;
+      windowManager.bspwm.enable = true;
+      layout = "us";
+      xkbOptions = "eurosign:e";
+    };
+    keybase.enable = true;
+    kbfs.enable = true;
+    openssh.enable = true;
+  };
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.max = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
+    shell = pkgs.zsh;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    htop direnv stow wget vim neovim barrier file mkpasswd neofetch
+    firefox chromium
+
+    capture scrot vlc gimp
+
+    i3lock betterlockscreen xorg.xdpyinfo bc feh
+    polybar rofi rxvt-unicode alacritty
+
+    git
+
+    gnumake
+
+    gcc go
+    ghc stack
+
+    docker-compose podman-compose
+
+    nodejs deno yarn nodePackages.typescript
+    insomnia cypress
+
+    vscodium
+
+    blender tiled
+
+    keybase-gui signal-desktop   
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+  
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ 22 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+  hardware.cpu.intel.updateMicrocode = true;
+
+  # Docker
+  virtualisation.docker.enable = true;
+
+  fonts.fonts = with pkgs; [
+    ubuntu_font_family
+    fira-code
+    noto-fonts-cjk 
+  ];
+
+  programs.zsh.enable = true;
+  programs.zsh.autosuggestions.enable = true;
+  programs.zsh.syntaxHighlighting.enable = true;
+
+  programs.mosh.enable = true;
+  programs.mosh.withUtempter = true;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+}
+
